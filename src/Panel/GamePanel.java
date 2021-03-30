f48b0081bd2182418df4d9c9b0884b0911492838
Tripenyazaraz <55177401+Tripenyazaraz@users.noreply.github.com>
2021-03-30 20:41:15 +0600

    commets changes

--- src/Panel/GamePanel.java
+++ src/Panel/GamePanel.java
@@ -15,17 +15,14 @@ public class GamePanel extends JPanel implements ActionListener {
     //тестовая ебала
     PixelInterface[] array = new PixelInterface[100];
 
-
-    //MainPanel panel;      //я не знаю зачем это но пока работает и без этого
-    Timer timerDraw = new Timer(20, this);                                  //объяявление таймера
-    Image bgImage = new ImageIcon("textures/texture_GamePanelBG.jpg").getImage();                 //текстура бг
+    Timer timerDraw = new Timer(20, this);                                      //объяявление таймера
+    Image bgImage = new ImageIcon("textures/texture_GamePanelBG.jpg").getImage();    //текстура бг
 
     //конструктор
     public GamePanel(MainPanel panel) {
         this.setSize(Display.panelGameWidth, Display.panelHeight);          //задать размер игровой панели
         this.setVisible(true);                                              //показать игровую панель
         timerDraw.start();                                                  //запуск таймера
-        //this.panel = panel;       //я не знаю зачем это но пока работает и без этого
 
         //действие при клике мышкой
         this.addMouseListener(new MouseAdapter() {
