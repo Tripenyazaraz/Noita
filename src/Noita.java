6be84b8de93e7f9ced6a2562457609bc2eea4b83
Tripenyazaraz <55177401+Tripenyazaraz@users.noreply.github.com>
2021-04-23 12:48:42 +0600

    choosePixel procedure delete
    its deleted cuz cringe, its usless

--- src/Noita.java
+++ src/Noita.java
@@ -5,23 +5,13 @@ import Pixel.Powder.Sand;
 
 public class Noita {
     //переменные
-    public static String currentPixelName = "unchosen";  //текущий выбранный пиксель
-    public static int io = 1;
+    public static String currentPixelName = "none";  //текущий выбранный пиксель
+    public static BasePixel currentPixel;
 
     public static void main(String[] args) {
         Engine mainEngine = new Engine();  //создание основного движка
 
-        choosePixel("Water");                     //выбор пикселя (это должно быть в кнопке)
+        currentPixelName = "Steam";                         //выбор пикселя (это должно быть в кнопке)
         mainEngine.generatePixel(currentPixelName, 1);   //создание пикселя (это должно быть при клике на поле)
-    }
-
-    //выбор пикселя (будет на кнопках в панели меню)
-    public static void choosePixel(String pixelName) {
-        switch (pixelName) {
-            case "Steam" -> currentPixelName = "Steam";
-            case "Water" -> currentPixelName = "Water";
-            case "Sand" -> currentPixelName = "Sand";
-            default -> System.out.println("Неправильное имя пикселя");
-        }
     }
 }
