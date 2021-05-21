54121e79f28742b8d7e2c1f32b36f6884ef405d3
Allakhyarov Eldar <tripenyazaraz@gmail.com>
2021-05-21 10:02:43 +0600

    Little fix in Console UI

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -1,10 +1,10 @@
 package Main;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class GUI extends Application{
     public static void main(String[] args) {
