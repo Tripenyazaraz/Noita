af1e79ce8ea829ffe7864d377c177af48df8e4c0
Tripenyazaraz <55177401+Tripenyazaraz@users.noreply.github.com>
2021-05-22 17:03:44 +0600

    Add icon - add getImage method - add createGUI method

--- src/Main/JavaFX_UI.java
+++ src/Main/JavaFX_UI.java
@@ -1,30 +1,46 @@
 package Main;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class JavaFX_UI extends Application{
+    final int GAME_PANEL_WIDTH = 800;
+    final int MENU_PANEL_WIDTH = 200;
+    final int HEIGHT = 800;
+
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage stage) {
-        // установка надписи
-        Text text = new Text("Hello from JavaFX!");
-        text.setLayoutY(80);    // установка положения надписи по оси Y
-        text.setLayoutX(100);   // установка положения надписи по оси X
+    public void start(Stage mainStage) {
+        createGUI(mainStage);
+    }
 
-        Group group = new Group(text);
+    public void createGUI(Stage mainStage) {
+        mainStage.getIcons().add(getImage("icon.png"));
+
+        Group group = new Group(); //вот сюда параметрами передавать что будет в окне
 
         Scene scene = new Scene(group);
-        stage.setScene(scene);
-        stage.setTitle("First Application");
-        stage.setWidth(300);
-        stage.setHeight(250);
-        stage.show();
+
+        mainStage.setScene(scene);
+        mainStage.setTitle("Noita");
+        mainStage.setWidth(GAME_PANEL_WIDTH + MENU_PANEL_WIDTH);
+        mainStage.setHeight(HEIGHT);
+        mainStage.setResizable(false);
+        mainStage.show();
+    }
+
+    //возвращает image объект.
+    public Image getImage(String path) {
+        InputStream imageStream = this.getClass().getResourceAsStream("/images/" + path);
+        return new Image(imageStream);
     }
 }
